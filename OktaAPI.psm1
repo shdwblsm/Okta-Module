0a4b6ed7cfdd11f5c84b5d2b80261dc404a69049
shdwblsm <sara.lovelace@gmail.com>
2023-02-07 11:51:46 -0600

    Added basic function stubs to OktaAPI

---
+++ OktaAPI.psm1
@@ -0,0 +1,23 @@
+function Get-OktaUser {
+    <#
+    .SYNOPSIS
+    Gets an Okta User from an Okta Organization Tenant
+    
+    .DESCRIPTION
+    This command is a basic pull of a user's Okta profile based on different information provided.
+    
+    .EXAMPLE
+    An example
+    
+    .NOTES
+    General notes
+    #>
+}
+
+function Get-federatedIdP {}
+
+function New-OktaUser {}
+
+function Deactivate-OktaUser {}
+
+function Remove-OktaUser {}
